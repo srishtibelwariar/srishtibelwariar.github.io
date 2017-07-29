ea30d893178098ebc315a5aa84a6abcd7d2dbed7
Srishti Belwariar <sb2355@cornell.edu>
2017-07-29 16:17:57 -0400

    major changes again

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+		function codeshow() {
+			document.getElementById('wopt').innerHTML="Projects";
+		}
+		
+		function musicshow() {
+			document.getElementById('wopt').innerHTML="Sitar Music";
+		}
+		
+		function orgshow() {
+			document.getElementById('wopt').innerHTML="Involvements";
+		}
+		
+		function wlabelhide() {
+			document.getElementById('wopt').innerHTML=" ";
+		}
+		
+		
+		function linkedinshow() {
+			document.getElementById('copt').innerHTML="Linked In";
+		}
+		
+		function githubshow() {
+			document.getElementById('copt').innerHTML="Git Hub";
+		}
+		
+		function emailshow() {
+			document.getElementById('copt').innerHTML="Email";
+		}
+		
+		function clabelhide() {
+			document.getElementById('copt').innerHTML=" ";
+		}
