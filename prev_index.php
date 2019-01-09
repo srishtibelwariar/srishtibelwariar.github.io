46f43f0d7bf12d0638f36a61cbd913c4eb848535
Srishti Belwariar <sb2355@cornell.edu>
2019-01-09 09:44:37 -0500

    hexagon grid menu

---
+++ prev_index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+		<head>
+				<!-- Metadata -->
+				<meta charset="UTF-8" />
+				<meta http-equiv="x-ua-compatible" content="ie=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+
+				<title>Srishti Belwariar</title>
+				<!-- <link rel="icon" href="images/favicon.png"> -->
+
+				<!-- Style -->
+				<link rel="stylesheet" href="main.css">
+				<link href="https://fonts.googleapis.com/css?family=Raleway:300" rel="stylesheet">
+
+
+				<!-- Scripts -->
+				<!-- Other Header Shit -->
+		</head>
+
+
+		<body>
+				<div class="page_container">
+					<div class="header_container">
+								<h2>HEADER</h2>
+								<img class="sd" src="cofolios.png" style="width: 500px;">
+						</div>
+
+						<div class="highlights_container">
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+								<div class="highlight">
+										<img src="bash.png" alt="ga">
+								</div>
+<!--
+								<div class="honeycomb_unit">
+									<div class="honeycomb_inside">
+										<img src="bash.png" alt="ga">
+									</div>
+								</div>
+
+								<div class="honeycomb_unit">
+									<div class="honeycomb_inside">
+										<img src="bash.png" alt="ga">
+									</div>
+								</div>
+
+								<div class="honeycomb_unit">
+									<div class="honeycomb_inside">
+										<img src="bash.png" alt="ga">
+									</div>
+								</div>
+
+								<div class="honeycomb_unit">
+									<div class="honeycomb_inside">
+										<img src="bash.png" alt="ga">
+									</div>
+								</div>
+
+								<div class="honeycomb_unit">
+									<div class="honeycomb_inside">
+										<img src="bash.png" alt="ga">
+									</div>
+								</div>
+
+<div class="honeycomb_unit">
+	<div class="honeycomb_inside">
+		<img src="bash.png" alt="ga">
+	</div> -->
+</div>
+
+
+
+
+						</div>
+
+
+				</div>
+		</body>
+</html>
